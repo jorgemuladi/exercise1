d90ad7f2daa8553eb7caa45945f645bb1f981a85
Jorge Muladi <jorgeosattimuladi@gmail.com>
2024-09-03 13:53:01 +0700

    feat : initial project

---
+++ src/main/java/org/example/Question1.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Question1 {
+
+    int length = 5;
+    int width = 3;
+
+
+    public int calAreaRectangle(int l, int w) {
+        return l * w;
+    }
+}
+
---
+++ src/main/java/org/example/Question2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Question2 {
+
+    double radius = 5;
+
+    public double calDiameter(){
+
+        return radius * 2;
+
+    }
+}
